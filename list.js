4bdb1892cd2ba6d7e85d87d0f53c4535283f9dfb
Angel06a <ug25228@gmail.com>
2025-03-16 18:21:40 -0500

    Update list.js

--- list.js
+++ list.js
@@ -62,7 +62,6 @@ Juegos Android=(Juego) (Android)=:
   -Paladog "https://www.mediafire.com/file/1xbi8es1hh3sscx/Paladog_2.2.0.apk/file"
   -Pureya "https://www.mediafire.com/file/7dwqkchjm2gxbl3/Pureya_1.0.15.apk/file"
   -Run & Gun: Banditos "https://www.mediafire.com/file/cu23w1yl9t4qp64/Run_%2526_Gun_BANDITOS_1.3.2.apk/file"
-  -Stickman Revenge 2 "https://www.mediafire.com/file/ld5rm53np2incot/Stickman_Revenge_2.apk/file"
   -Super Mario 64 Port "https://github.com/coop-deluxe/sm64coopdx/releases"
   -The Legend of Zelda Port "https://github.com/Waterdish/Shipwright-Android/releases"
   -Zombie Avengers Dreamsky Stickman War "https://www.mediafire.com/file/bd1r8u5gsej8vvn/Zombie_Avengers_Dreamsky_Stickman_War.apk/file"
